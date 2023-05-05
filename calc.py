1ebb554828d8f52deaefe7c151aa8cbb6b79bb42
RFujinami <reonafuji@gmail.com>
2023-05-05 21:33:28 +0900

    UIの設計変更

--- calc.py
+++ calc.py
@@ -14,8 +14,13 @@ from flet import (
 class CalcApp(UserControl):
     def build(self):
         self.result = Text(value="0", size=30)
+        self.num = 0
+        self.is_new = False
+        self.operand_select = False
 
         return Container(
+            width=300,
+            height=390,
             content=Column(
                 controls=[
                     Row(controls=[self.result]),
@@ -26,14 +31,69 @@ class CalcApp(UserControl):
                         ElevatedButton(text="/",expand=1,on_click=self.button_click,data="/"),
                     ]),
                     Row(controls=[
-                        
-                    ])
+                        ElevatedButton(text="7",expand=1,on_click=self.button_click,data="7"),
+                        ElevatedButton(text="8",expand=1,on_click=self.button_click,data="8"),
+                        ElevatedButton(text="9",expand=1,on_click=self.button_click,data="9"),
+                        ElevatedButton(text="*",expand=1,on_click=self.button_click,data="*"),   
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text="4",expand=1,on_click=self.button_click,data="4"),
+                        ElevatedButton(text="5",expand=1,on_click=self.button_click,data="5"),
+                        ElevatedButton(text="6",expand=1,on_click=self.button_click,data="6"),
+                        ElevatedButton(text="-",expand=1,on_click=self.button_click,data="-"),
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text="1",expand=1,on_click=self.button_click,data="1"),
+                        ElevatedButton(text="2",expand=1,on_click=self.button_click,data="2"),
+                        ElevatedButton(text="3",expand=1,on_click=self.button_click,data="3"),
+                        ElevatedButton(text="+",expand=1,on_click=self.button_click,data="+"),
+                    ]),
+                    Row(controls=[
+                        ElevatedButton(text="0",expand=2,on_click=self.button_click,data="0"),
+                        ElevatedButton(text=".",expand=1,on_click=self.button_click,data="."),
+                        ElevatedButton(text="=",expand=1,on_click=self.button_click,data="="),
+                    ]),
                 ]
             )
         )
     
     def button_click(self, e):
-        pass
+        data = e.control.data
+
+        if(str.isdigit(data)):
+            self.num=int(data)
+            self.operand_select = False
+
+            if(self.operand_select):
+                pass
+
+            self.result.value = data
+            print(data)
+        elif(data == "+"):
+            self.operand_select = True
+        elif(data == "-"):
+            self.operand_select = True
+        elif(data == "*"):
+            self.operand_select = True
+        elif(data == "/"):
+            self.operand_select = True
+        elif(data == "%"):
+            self.operand_select = True
+        elif(data == "."):
+            pass
+        elif(data == "+/-"):
+            self.operand_select = True
+        elif(data == "="):
+            self.operand_select = True
+        else:
+            self.operand_select = False
+            self.result.value = 0
+
+        self.update()
+
+
+
+
     
 def main(page: ft.Page):
     page.title = "Calculator"
